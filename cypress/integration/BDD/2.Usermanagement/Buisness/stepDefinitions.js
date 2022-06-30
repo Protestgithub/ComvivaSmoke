5f7bb3395a5a139eb63cd93fae3fdbd0a3e92312
Protestgithub <99022635+Protestgithub@users.noreply.github.com>
2022-06-30 16:56:31 +0530

    Update stepDefinitions.js
    Changed Business URL

--- cypress/integration/BDD/2.Usermanagement/Buisness/stepDefinitions.js
+++ cypress/integration/BDD/2.Usermanagement/Buisness/stepDefinitions.js
@@ -96,7 +96,7 @@ Given('Login into Mobiquity Portal as Super admin Maker', function () {
 
 Given('Login into Mobiquity Portal as Business admin User', function () {
   cy.launchURL(Cypress.env('Adminurl'))
-  cy.launchURL(Cypress.env('Businessurl'))
+  cy.visit(Cypress.env("Adminurl") + "/Business"
   cy.wait(8000)
   cy.readFile('cypress/fixtures/userData/BusinessUserSuspensionData.json').then((data)=>{
     var SuspendedId
@@ -109,7 +109,7 @@ pageLogin.getUserLoginMessage().should('contain',this.data1.ErrorMessageLogin)
 
 Given('Login into Mobiquity Portal as Business admin User1', function () {
   cy.launchURL(Cypress.env('Adminurl'))
-  cy.launchURL(Cypress.env('Businessurl'))
+  cy.visit(Cypress.env("Adminurl") + "/Business"
   cy.wait(8000)
   cy.readFile('cypress/fixtures/userData/BusinessUserSuspensionData.json').then((data)=>{
     var SuspendedId
