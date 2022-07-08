b84f0a59309570eb99e5edd1e293f5cd60aa448c
Kalyani Modali <kalyani.modali@comviva.com>
2022-07-08 14:31:32 +0530

    #FRI-000 | Updated given Statements| FIX

--- cypress/integration/BDD/AuthorizationProfile/AuthorizationProfManagement/authorizationProfStepDefinition.js
+++ cypress/integration/BDD/AuthorizationProfile/AuthorizationProfManagement/authorizationProfStepDefinition.js
@@ -50,18 +50,36 @@ function getRandomName() {
 //----------------Launch Mobiquity Portal URL and Login--------------------------------------
 
 
-Given('Login into Mobiquity Portal as System admin User', function () {
+Given('Login into Mobiquity Portal as System admin Maker', function () {
   cy.launchURL(Cypress.env('Adminurl'))
-  cy.login(this.data1.sysAdmin1.sysAdminUser1, this.data1.sysAdmin1.sysAdminPwd1)
+  cy.SysAdminlogin()
   cy.wait(2000)
-  cy.checkWelcomeText(this.data1.networkAdminWelcomeText)
+  cy.checkWelcomeText(this.data2.networkAdminWelcomeText)
 })
-
-Given('Login into Mobiquity Portal as another System admin User', function () {
+Given('Login into Mobiquity Portal as System admin Checker1', function () {
   cy.launchURL(Cypress.env('Adminurl'))
-  cy.login(this.data1.sysAdmin2.sysAdminUser1, this.data1.sysAdmin2.sysAdminPwd1)
-  cy.checkWelcomeText(this.data1.networkAdminWelcomeText)
-
+  cy.SysAdminlogin2()
+  cy.wait(2000)
+  cy.checkWelcomeText(this.data2.networkAdminWelcomeText)
+})
+Given('Login into Mobiquity Portal as System admin Maker after Logout', function () {
+  cy.launchURL(Cypress.env('Adminurl'))
+  cy.wait(2000)
+  cy.SysAdminlogin()
+  cy.wait(2000)
+  cy.checkWelcomeText(this.data2.networkAdminWelcomeText)
+})
+Given('Login into Mobiquity Portal as another System admin Checker1 after logout', function () {
+  cy.launchURL(Cypress.env('Adminurl'))
+  cy.wait(2000)
+  cy.SysAdminlogin2()
+  cy.wait(2000)
+  cy.checkWelcomeText(this.data2.networkAdminWelcomeText)
+})
+Then('Logout', function(){
+  welcomePage.getUserMenu().click()
+  welcomePage.getLogoutButton().click()
+  welcomePage.getLogoutYesButton().click()  
 })
 
 //------------------------ Authorization Profile Management----------------------------------
