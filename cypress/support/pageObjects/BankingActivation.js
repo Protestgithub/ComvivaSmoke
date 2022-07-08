99209f05d59b1571eb484417ce5dbf6dbd396bfb
Kalyani Modali <kalyani.modali@comviva.com>
2022-07-08 14:53:05 +0530

    #FRI-000 | Added new file for bankpageobject| FIX

---
+++ cypress/support/pageObjects/BankingActivation.js
@@ -0,0 +1,26 @@
+class BankingActivation
+{
+    getEnterCIFNumber(){
+        return cy.iframe().find('[class="mat-form-field-infix ng-tns-c78-0"]')
+    }
+    getSearchButton(){
+        return cy.iframe().find('button[type="submit"]').eq(0).contains(' Search ')
+    }
+    getMobileBankingButton(){
+        return cy.iframe().find('button.action-btn.btn.btn-primary').eq(1).contains('Activate Mobile')
+    }
+    getInternetBankingButton(){
+        return cy.iframe().find('button.action-btn.btn.btn-primary').eq(2).contains('Activate Internet')
+    }
+    getConfirmButton(){
+        return cy.iframe().find('div>button[class="unblock-btn btn"]')
+    }
+    getMobActivatedMsg(){
+        return cy.iframe().find('[type="button"]').eq(1)
+    }
+    getInternetActivatedMsg(){
+        return cy.iframe().find('[type="button"]').eq(2)
+    }
+   
+}
+export default BankingActivation
