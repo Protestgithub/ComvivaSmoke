06c7a4f8f82dd0a3d49f371ac3ca81d1879fda27
Kalyani Modali <kalyani.modali@comviva.com>
2022-07-13 15:01:51 +0530

    #WED-000 | Added new file | FIX

---
+++ cypress/fixtures/OrderDetailsCreation.js
@@ -0,0 +1,12 @@
+{
+    "Currency": {
+        "value": "Indian Rupee"
+    },
+    "wallettype": {
+        "value": "Normal"
+    },
+    "accountnumber": {
+        "number": "ICICI - 7470771987261649"
+    }
+ 
+}
