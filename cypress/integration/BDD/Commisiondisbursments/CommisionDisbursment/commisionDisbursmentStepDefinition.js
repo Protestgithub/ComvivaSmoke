0ac6b5d7fbefc8034084c6d050cc10c81e4bddc0
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-15 17:25:50 +0530

    #FRI-000 | Added Before Hook to skip rest on first fail | NEW

--- cypress/integration/BDD/Commisiondisbursments/CommisionDisbursment/commisionDisbursmentStepDefinition.js
+++ cypress/integration/BDD/Commisiondisbursments/CommisionDisbursment/commisionDisbursmentStepDefinition.js
@@ -36,7 +36,19 @@ Before(() => {
   cy.fixture('CommisionDisbursment').then(function (data6) {
     this.data6 = data6;
   })
-
+  if ( Cypress.browser.isHeadless ) {
+    cy.clearCookie('shouldStop')
+  } else {
+    cy.getCookie('shouldStop').then(cookie => {
+      if (
+        cookie &&
+        typeof cookie === 'object' &&
+        cookie.value === 'true'
+      ) {
+        Cypress.runner.stop();
+      }
+    })
+  }
 
 });
 
