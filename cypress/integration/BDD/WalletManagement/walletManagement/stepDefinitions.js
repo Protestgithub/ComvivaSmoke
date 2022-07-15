70b571f396222b4cd8e628dce1af9ac64bfe521d
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-15 17:34:30 +0530

    #FRI-000 | Added Before Hook to skip rest on first fail | NEW

--- cypress/integration/BDD/WalletManagement/walletManagement/stepDefinitions.js
+++ cypress/integration/BDD/WalletManagement/walletManagement/stepDefinitions.js
@@ -33,9 +33,21 @@ Before(() => {
   cy.fixture('UserManagement').then(function (data2) {
     this.data2 = data2;
   })
+  if ( Cypress.browser.isHeadless ) {
+    cy.clearCookie('shouldStop')
+  } else {
+    cy.getCookie('shouldStop').then(cookie => {
+      if (
+        cookie &&
+        typeof cookie === 'object' &&
+        cookie.value === 'true'
+      ) {
+        Cypress.runner.stop();
+      }
+    })
+  }
 
-
-})
+});
 
 //--------------------------------------Login-------------------------------------------------------
 Given('Login into Mobiquity Portal as System admin User', function () {
