9db8d166e52ea1d15ad74b107faf067f524d3b3a
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-15 17:24:15 +0530

    #FRI-000 | Added Before Hook to skip rest on first fail | NEW

--- cypress/integration/BDD/BulkPayout/BulkPayout/stepdefinition.js
+++ cypress/integration/BDD/BulkPayout/BulkPayout/stepdefinition.js
@@ -77,7 +77,19 @@ Before(() => {
        this.data9=data9
    })
 
-    
+      if ( Cypress.browser.isHeadless ) {
+    cy.clearCookie('shouldStop')
+  } else {
+    cy.getCookie('shouldStop').then(cookie => {
+      if (
+        cookie &&
+        typeof cookie === 'object' &&
+        cookie.value === 'true'
+      ) {
+        Cypress.runner.stop();
+      }
+    })
+  }
 });
  -
 //----------------Test Scripts---------------------------------------------------------------------------
