48c2323caec162373446a6a53e14e8ebcd756f18
Ravi Narendra <ravi.narendra@comviva.com>
2022-07-19 11:10:31 +0530

    #TUE-000 | Update Changes| FIX

--- cypress/integration/BDD/WalletManagement/walletManagement/stepDefinitions.js
+++ cypress/integration/BDD/WalletManagement/walletManagement/stepDefinitions.js
@@ -33,24 +33,11 @@ Before(() => {
   cy.fixture('UserManagement').then(function (data2) {
     this.data2 = data2;
   })
-  if ( Cypress.browser.isHeadless ) {
-    cy.clearCookie('shouldStop')
-  } else {
-    cy.getCookie('shouldStop').then(cookie => {
-      if (
-        cookie &&
-        typeof cookie === 'object' &&
-        cookie.value === 'true'
-      ) {
-        Cypress.runner.stop();
-      }
-    })
-  }
 
 });
 
 //--------------------------------------Login-------------------------------------------------------
-Given('Login into Mobiquity Portal as System admin User', function () {
+Given('Login into Mobiquity Portal as System admin Maker', function () {
   cy.launchURL(Cypress.env('Adminurl'))
   cy.SysAdminlogin()
   cy.wait(2000)
