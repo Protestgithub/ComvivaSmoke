1032ed7b7150484992c950c4efbc8736401dcbc5
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-22 15:15:42 +0530

    #FRI | Updated step definitions | NEW

--- cypress/integration/BDD/1.PreRequisite/8.ProfilesCreation/SecurityProfilesCreation/stepdefinition.js
+++ cypress/integration/BDD/1.PreRequisite/8.ProfilesCreation/SecurityProfilesCreation/stepdefinition.js
@@ -41,19 +41,6 @@ Before(() => {
     this.data6 = data6;
   })
   
- if ( Cypress.browser.isHeadless ) {
-    cy.clearCookie('shouldStop')
-  } else {
-    cy.getCookie('shouldStop').then(cookie => {
-      if (
-        cookie &&
-        typeof cookie === 'object' &&
-        cookie.value === 'true'
-      ) {
-        Cypress.runner.stop();
-      }
-    })
-  }
 }); 
 
 //---------------------------------------------Login----------------------------------------------------
