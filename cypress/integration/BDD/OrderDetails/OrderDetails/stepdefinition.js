848714c7548613bc7abb5e9e78dab938cdcf58d1
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-22 15:25:35 +0530

    #FRI-000 | Updated step definitions | NEW

--- cypress/integration/BDD/OrderDetails/OrderDetails/stepdefinition.js
+++ cypress/integration/BDD/OrderDetails/OrderDetails/stepdefinition.js
@@ -51,19 +51,7 @@ Before(() => {
   cy.fixture('UserManagement').then(function (data2) {
     this.data2 = data2;
   })
-    if ( Cypress.browser.isHeadless ) {
-    cy.clearCookie('shouldStop')
-  } else {
-    cy.getCookie('shouldStop').then(cookie => {
-      if (
-        cookie &&
-        typeof cookie === 'object' &&
-        cookie.value === 'true'
-      ) {
-        Cypress.runner.stop();
-      }
-    })
-  }
+
 });
 //----------------Test Scripts---------------------------------------------------------------------------
 
