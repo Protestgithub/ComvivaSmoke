febb32c96ccd5edc9c5c52bea9aebdde52c2c5d8
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-22 15:32:08 +0530

    #FRI-000 | Updated step definitions | NEW

--- cypress/integration/BDD/1.PreRequisite/2.BankCreation/BankCreation/stepDefinitions.js
+++ cypress/integration/BDD/1.PreRequisite/2.BankCreation/BankCreation/stepDefinitions.js
@@ -84,19 +84,7 @@ Before(() => {
   {
      this.data03 = data03;
   })
-  if ( Cypress.browser.isHeadless ) {
-    cy.clearCookie('shouldStop')
-  } else {
-    cy.getCookie('shouldStop').then(cookie => {
-      if (
-        cookie &&
-        typeof cookie === 'object' &&
-        cookie.value === 'true'
-      ) {
-        Cypress.runner.stop();
-      }
-    })
-  }
+
 }); 
 
 //---------------------------------------------Login----------------------------------------------------
