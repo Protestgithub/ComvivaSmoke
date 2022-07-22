acc8c4077e275b7926f491984ab9dc6a479a27bf
Shri Vidhya <shri.vidhya@comviva.com>
2022-07-22 15:30:41 +0530

    #FRI | Updated step definitions | NEW

--- cypress/integration/BDD/TransactionCorrection/TransactionCorrection/stepdefinition.js
+++ cypress/integration/BDD/TransactionCorrection/TransactionCorrection/stepdefinition.js
@@ -59,19 +59,7 @@ Before(() => {
   cy.fixture('TransactionCorrection').then(function (data6) {
     this.data6 = data6;
   })
-    if ( Cypress.browser.isHeadless ) {
-    cy.clearCookie('shouldStop')
-  } else {
-    cy.getCookie('shouldStop').then(cookie => {
-      if (
-        cookie &&
-        typeof cookie === 'object' &&
-        cookie.value === 'true'
-      ) {
-        Cypress.runner.stop();
-      }
-    })
-  }
+
 });
 
 //---------------------------------------------System Admin Login----------------------------------------------------
