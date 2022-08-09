9bcf7e4720240f2ccca1a2485647df3619c28b4a
Kalyani Modali <kalyani.modali@comviva.com>
2022-08-09 10:07:43 +0530

    #TUE-000 |Updated the changes in banking channel | FIX

--- cypress/integration/BDD/Tests/BankingChannelActivation/BankingActivation/StepDefinition.js
+++ cypress/integration/BDD/Tests/BankingChannelActivation/BankingActivation/StepDefinition.js
@@ -7,6 +7,8 @@ import { Given, When, Then, And, Before } from "cypress-cucumber-preprocessor/st
 import homePage from '../../../../../support/pageObjects/homePage';
 import "../../../../../support/bankingActivationCommands";
 import BankingActivation from '../../../../../support/pageObjects/BankingActivation';
+import "../../../../../support/commands";
+
 //----------------Object Declaration----------------------------------------------------------
 const welcomePage = new homePage()
 const bankingActivationPage = new BankingActivation()
@@ -111,19 +113,31 @@ Then('verify admin is able to see the Activated Message', function () {
 //--------TC_158-----------------------------------------------------------------------------------------
 And('Enter the Full KYC mobile number and search for the user', function () {
   cy.wait(2000)
-  bankingActivationPage.getEnterCIFNumber().type(this.data6.subscriberMobileBankingActivation, { force: true })
+  bankingActivationPage.getEnterCIFNumber().type(this.data6.subscriberMobile, { force: true })
   cy.wait(2000)
   bankingActivationPage.getSearchButton().click()
 })
-  And('Click on next and activate mobile Banking', function(){
+And('Click on next and activate mobile Banking', function () {
   bankingActivationPage.getNextBtn().click({ force: true })
   bankingActivationPage.getNextBtn1().click({ force: true })
-  bankingActivationPage.getNextBtn2().click({ force: true })
+  cy.OTP1(Cypress.env('apiBaseURL'), Cypress.env('apiURL'))
+  cy.wait(2000)
+  bankingActivationPage.getMobileBankingButton().click({ force: true })
+  bankingActivationPage.getConfirmButton().contains('Confirm').click({force:true})
+
 })
 
 //--------TC_159-----------------------------------------------------------------------------------------
 
+And('Click on next and activate internet Banking', function () {
+  bankingActivationPage.getNextBtn().click({ force: true })
+  bankingActivationPage.getNextBtn1().click({ force: true })
+  cy.OTP1(Cypress.env('apiBaseURL'), Cypress.env('apiURL'))
+  cy.wait(2000)
+  bankingActivationPage.getInternetBankingButton().click({ force: true })
+  bankingActivationPage.getConfirmButton().contains('Confirm').click({force:true})
 
+})
 //--------TC_160-----------------------------------------------------------------------------------------
 And('Enter the mobile or CIF number which is not registered and search for the user', function () {
   cy.wait(2000)
@@ -132,7 +146,6 @@ And('Enter the mobile or CIF number which is not registered and search for the u
   bankingActivationPage.getSearchButton().click()
 })
 
-Then('verify admin is able to see the Message mobile or CIF number does not exist in the system',function() {
+Then('verify admin is able to see the Message mobile or CIF number does not exist in the system', function () {
   bankingActivationPage.getInvalidNumber().should('contain.text', ' is invalid. Next ')
-  })
-  
+})
