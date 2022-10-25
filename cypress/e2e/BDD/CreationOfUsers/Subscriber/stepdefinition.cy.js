60a03a4bc6bf47577fb6f13fe7a6e7dd29e123b7
Protestgithub <99022635+Protestgithub@users.noreply.github.com>
2022-10-25 18:17:49 +0530

    Update stepdefinition.cy.js

---
+++ cypress/e2e/BDD/CreationOfUsers/Subscriber/stepdefinition.cy.js
@@ -0,0 +1,52 @@
+/// <reference types="Cypress" />
+/// <reference types = "Cypress-iframe"/>
+
+//----------------Imports---------------------------------------------------------------------
+import 'cypress-iframe'
+import { Given, When, Then, And, Before } from "@badeball/cypress-cucumber-preprocessor";
+import loginPage from '../../../../../support/pageObjects/loginPage';
+import homePage from '../../../../../support/pageObjects/homePage';
+import "../../../../../support/utils/Generic";
+import "../../../../../support/utils/subscriberCommands";
+import register from '../../../../../support/pageObjects/UserManagement/register';
+import approvals from '../../../../../support/pageObjects/UserManagement/approvals';
+import manageUsers from '../../../../../support/pageObjects/UserManagement/manageUsers';
+import myActivity from '../../../../../support/pageObjects/MyActivity/myActivity';
+import { recurse } from 'cypress-recurse';
+
+//----------------Object Declaration-----------------------------------------------------------------
+
+const pageLogin = new loginPage()
+const welcomePage = new homePage()
+const registerPage = new register()
+const approvalPage = new approvals()
+const myActivityPage = new myActivity()
+const manageUsersPage = new manageUsers()
+const uuid = () => Cypress._.random(1e8)
+const SubMob = 'userData/subscriberReg.json'
+var lid, eid, CIF, mobile, Mobile, Submobile, loginId, KycValue, name
+mobile = "77" + uuid()
+Mobile = "77" + uuid()
+var subRegistration = 'cypress/fixtures/userData/subscriberReg.json'
+const timestamp = (new Date).getTime()
+  KycValue = "A" + timestamp
+
+function getRandomName() {
+    name = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < 5; i++)
+      name += possible.charAt(Math.floor(Math.random() * possible.length));
+    return name;  
+  }
+
+And('Select User type as Subscriber and click on Subscribers', function () {
+  welcomePage.getUserManagementOption().scrollIntoView()
+  welcomePage.getUserManagementOption().click()
+  welcomePage.getRegisterOption().click()
+  cy.wait(2000)
+  registerPage.getregisterPageTitle().should('have.text', this.data2.registerPageTitle)
+  registerPage.getSelectSubUserTypeTab().contains(this.data2.subPersonalInfo.subUserType).click({ force: true })
+  registerPage.getSelectSubUserTypeTab().contains(this.data2.subPersonalInfo.subUserType).focused()
+  registerPage.getUserRole().contains(this.data2.subPersonalInfo.subUserRole).click({ force: true })
+  registerPage.getRegistrationMode().eq(0).click({ force: true })
+})
